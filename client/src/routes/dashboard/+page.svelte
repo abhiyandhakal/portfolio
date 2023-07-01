24fdad7e217b537d1cb0c16083a9fd1cd7b50ccd
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-07-01 12:16:01 +0545

    added @types/uuid

---
+++ client/src/routes/dashboard/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import "../../styles/style.scss";
+</script>
+
+<form action="/api/dashboard" method="post">
+	<input type="file" name="file" />
+	<button type="submit">Upload</button>
+</form>
