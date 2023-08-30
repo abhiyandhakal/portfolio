6ab0a16c271002668aa0b304ee8fd39673379f57
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-08-30 20:08:20 +0545

    add vercel web analyticsg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Footer from "$components/footer.svelte";
 	import Nav from "$components/nav.svelte";
+	import { dev } from "$app/environment";
+	import { inject } from "@vercel/analytics";
+	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <header>
