4fcf7d89104d3f3ff3a68809b00014a3493f5c20
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-03-29 10:27:24 +0545

    setup mdsvex and vite for markdown files

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: [".md"]
+		})
+	],
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
@@ -23,7 +29,8 @@ const config = {
 			$lib: "./src/lib",
 			$data: "./src/data"
 		}
-	}
+	},
+	extensions: [".svelte", ".md"]
 };
 
 export default config;
