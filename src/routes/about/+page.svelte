03651e87a311e2f1544964678d9c51a6a34d0d38
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-07-25 09:52:45 +0545

    update semester to 5th

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -163,7 +163,7 @@
 	Hi<iconify-icon icon="noto:waving-hand" />! I'm <b>Abhiyan Dhakal</b>, a tech eithusiast with a
 	diverse skillset. I am currently studying in
 	<a href="https://soe.ku.edu.np/" target="_blank">Kathmandu University, School of Engineering</a>,
-	pursuing Computer Engineering, currently in 4<sup>th</sup> semester.
+	pursuing Computer Engineering, currently in 5<sup>th</sup> semester.
 
 	<section aria-labelledby="full-stack">
 		<h2 class="h2" id="full-stack">Full Stack Web Developer:</h2>
